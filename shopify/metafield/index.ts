462bcd0428f4190c863413f9ebf5e49501b45d5d
Yen Px <yenpx.bsscommerce.com>
2025-05-27 09:18:28 +0700

    update

--- shopify/metafield/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './setMetafields';
-export * from './getMetafieldById';
-export * from './definitionMetafields';
-export * from './updateMetafieldsDefinition';
-
-
--- shopify/metaobject/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './getMetaobjectDefinitionByType';
-export * from './metaobjectCreate';
-export * from './metaobjectDefinitionAddFields';
-export * from './metaobjectDefinitionCreate';
-export * from './metaobjectDefinitionDelete';
-export * from './metaobjectUpdate';
-
